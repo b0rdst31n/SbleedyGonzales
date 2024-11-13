16fe7f87dadc4c779c76ad8c77cde155fc74d542
b0rdst31n <b0rdst31n@gmail.com>
2024-11-13 15:42:30 +0100

    Update sbleedy_installer.sh

--- helpers/sbleedy_installer.sh
+++ helpers/sbleedy_installer.sh
@@ -1,152 +1,134 @@
 #!/bin/bash
 
-VENV_DIR="${PWD}"
-SUCCESS=()
-ERRORS=()
-
-# Function to print colored messages
-print_message() {
-    COLOR=$1
-    MESSAGE=$2
-    RESET='\033[0m'
-    echo -e "${COLOR}${MESSAGE}${RESET}"
-}
-
-# Colors for messages
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-
-# Function to check if a Python version exists
-check_python_version() {
-    if command -v $1 &> /dev/null
-    then
-        print_message $GREEN "$1 is installed."
-        SUCCESS+=("$1 is installed.")
-    else
-        print_message $RED "$1 is not installed."
-        ERRORS+=("$1 is not installed.")
-    fi
-}
-
-# Ask for user confirmation to proceed
-confirm() {
-    read -p "$1 (y/n): " choice
-    case "$choice" in 
-      y|Y ) return 0;;
-      * ) return 1;;
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --http_proxy)
+            http_proxy="$2"
+            shift 2
+            ;;
+        --https_proxy)
+            https_proxy="$2"
+            shift 2
+            ;;
+        --no_proxy)
+            no_proxy="$2"
+            shift 2
+            ;;
+        --pip_proxy)
+            PIP_PROXY="$2"
+            shift 2
+            ;;
+        --pip_cert)
+            PIP_CERT="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
     esac
-}
+done
 
-# Check and optionally install Python versions
-check_and_install_python() {
-    local python_version=$1
-    local install_cmd=$2
+export http_proxy
+export https_proxy
+export no_proxy
+export PIP_PROXY
 
-    if ! command -v $python_version &> /dev/null; then
-        if confirm "$python_version is not installed. Would you like to install it?"; then
-            eval $install_cmd
-            if command -v $python_version &> /dev/null; then
-                print_message $GREEN "$python_version installed successfully."
-                SUCCESS+=("$python_version installed.")
-            else
-                print_message $RED "Failed to install $python_version."
-                ERRORS+=("Failed to install $python_version.")
-            fi
-        else
-            print_message $RED "Skipping $python_version installation."
-            ERRORS+=("Skipped $python_version installation.")
-        fi
-    fi
-}
+if [ -n "$http_proxy" ]; then
+    echo "Acquire::http::Proxy \"$http_proxy\";" | sudo tee /etc/apt/apt.conf.d/01proxy
+fi
 
-# Install Python 3.10 if not installed
-check_and_install_python "python3.10" "sudo apt update && sudo apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev libffi-dev curl && cd /usr/src && sudo curl -O https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz && sudo tar xzf Python-3.10.0.tgz && cd Python-3.10.0 && sudo ./configure --enable-optimizations && sudo make -j$(nproc) && sudo make altinstall && sudo rm -rf /usr/src/Python-3.10.0*"
+sudo apt-get -y update #&& sudo apt-get -y upgrade
 
-# Install Python 2.7 if not installed
-check_and_install_python "python2.7" "sudo apt update && sudo apt install -y python2.7"
+sudo apt-get install -y tzdata software-properties-common build-essential libssl-dev zlib1g-dev libncurses5-dev \
+     libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev libffi-dev \
+     curl python2.7 python2-dev gcc wget g++ make git bluez bluetooth usbutils libbluetooth-dev cmake libcairo2-dev \
+     pkg-config libgirepository1.0-dev libdbus-1-dev bluez-tools python3-cairo-dev rfkill meson patchelf bluez adb python-is-python3
 
-# Install pip and virtualenv for Python versions if installed
-install_pip_and_virtualenv() {
-    local python_version=$1
-    local get_pip_url=$2
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
 
-    if command -v $python_version &> /dev/null; then
-        curl -s $get_pip_url -o get-pip.py
-        $python_version get-pip.py && $python_version -m pip install virtualenv
-        rm -f get-pip.py
-        print_message $GREEN "Pip and virtualenv installed for $python_version."
-        SUCCESS+=("Pip and virtualenv installed for $python_version.")
-    else
-        ERRORS+=("$python_version is not available for pip/virtualenv installation.")
-    fi
-}
+MAIN_DIR="$(dirname "$(realpath "$0")")/.."
+MAIN_DIR="$(realpath "${MAIN_DIR}")"
 
-install_pip_and_virtualenv "python3.10" "https://bootstrap.pypa.io/get-pip.py"
-install_pip_and_virtualenv "python2.7" "https://bootstrap.pypa.io/pip/2.7/get-pip.py"
+TMP_DIR=$(mktemp -d)
+cd $TMP_DIR
 
-# Check and create virtual environments
-create_virtualenv() {
-    local venv_name=$1
-    local python_version=$2
+if ! command -v python3.10 &> /dev/null; then
+    wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+    tar -xf Python-3.10.0.tgz
+    cd Python-3.10.0
+    ./configure --enable-optimizations
+    make -j $(nproc)
+    sudo make altinstall
+    cd ..
+    rm -rf Python-3.10.0*
+else
+    echo "Python 3.10 is already installed."
+fi
 
-    if [ ! -d "$VENV_DIR/$venv_name" ]; then
-        if command -v $python_version &> /dev/null; then
-            $python_version -m virtualenv -p $python_version "$VENV_DIR/$venv_name"
-            print_message $GREEN "Created virtual environment $venv_name with $python_version at $VENV_DIR."
-            SUCCESS+=("$venv_name created with $python_version.")
-        else
-            print_message $RED "$python_version is not available for virtual environment creation."
-            ERRORS+=("$venv_name creation failed.")
-        fi
-    else
-        print_message $GREEN "Virtual environment $venv_name already exists."
-        SUCCESS+=("$venv_name already exists.")
-    fi
-}
+python2.7 --version
+python3.10 --version
 
-create_virtualenv "venv3" "python3.10"
-create_virtualenv "venv2" "python2.7"
+python2.7 -m pip config set global.cert "$PIP_CERT"
+python3.10 -m pip config set global.cert "$PIP_CERT"
 
-# Install requirements in venv2 (Python 2.7)
-if [ -d "$VENV_DIR/venv2" ]; then
-    source "$VENV_DIR/venv2/bin/activate"
-    cd "$VENV_DIR/modules/sweyntooth"
-    sudo apt install python2-dev gcc g++ make -y
-    pip install -r requirements.txt
-    cd ./libs/smp_server/ && make clean && make build && make install && cd ../../
-    SUCCESS+=("Dependencies installed in venv2.")
+if ! command -v pip2.7 &> /dev/null; then
+    wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -O get-pip.py
+    wget https://files.pythonhosted.org/packages/27/79/8a850fe3496446ff0d584327ae44e7500daf6764ca1a382d2d02789accf7/pip-20.3.4-py2.py3-none-any.whl
+    wget https://files.pythonhosted.org/packages/e1/b7/182161210a13158cd3ccc41ee19aadef54496b74f2817cc147006ec932b4/setuptools-44.1.1-py2.py3-none-any.whl
+    wget https://files.pythonhosted.org/packages/27/d6/003e593296a85fd6ed616ed962795b2f87709c3eee2bca4f6d0fe55c6d00/wheel-0.37.1-py2.py3-none-any.whl
+
+    python2.7 get-pip.py "pip-20.3.4-py2.py3-none-any.whl" "setuptools-44.1.1-py2.py3-none-any.whl" "wheel-0.37.1-py2.py3-none-any.whl" --index-url http://pypi.python.org/simple/ --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
+    python2.7 -m pip install virtualenv
+    rm -f get-pip.py
+else
+    echo "Pip for Python 2.7 is already installed."
+fi
+
+if ! command -v pip3.10 &> /dev/null; then
+    sudo apt-get install -y python3-pip
+    python3.10 -m pip install --upgrade pip --index-url http://pypi.python.org/simple/ --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
+    python3.10 -m pip install virtualenv
+else
+    echo "Pip for Python 3.10 is already installed."
+fi
+
+if [ ! -d "${MAIN_DIR}/venv3" ]; then
+    python3.10 -m virtualenv ${MAIN_DIR}/venv3
+else
+    source ${MAIN_DIR}/venv3/bin/activate
+    python3.10 -m pip install --upgrade pip
     deactivate
 fi
 
-# Install dependencies in venv3 (Python 3.10)
-if [ -d "$VENV_DIR/venv3" ]; then
-    source "$VENV_DIR/venv3/bin/activate"
-    git submodule update --init --recursive
-    if [ -d "$VENV_DIR/helpers/pybluez" ]; then
-        cd "$VENV_DIR/helpers/pybluez" && pip install . && cd ../../
-        SUCCESS+=("pybluez installed.")
-    else
-        print_message $RED "Error: helpers/pybluez directory not found."
-        ERRORS+=("helpers/pybluez directory not found.")
-    fi
-    if [ -d "$VENV_DIR/helpers/bluing" ]; then
-        cd "$VENV_DIR/helpers/bluing" && env CFLAGS="-lm" pip install . && cd ../../
-        SUCCESS+=("bluing installed.")
-    else
-        print_message $RED "Error: bluing directory not found."
-        ERRORS+=("bluing directory not found.")
-    fi
-    pip install .
-    SUCCESS+=("Sbleedy installed in venv3.")
+if [ ! -d "${MAIN_DIR}/venv2" ]; then
+    python2.7 -m virtualenv ${MAIN_DIR}/venv2
+else
+    source ${MAIN_DIR}/venv2/bin/activate
+    python2.7 -m pip install --upgrade pip
     deactivate
 fi
 
-# Summary of script completion
-echo
-echo "Summary of Installation:"
-for msg in "${SUCCESS[@]}"; do
-    print_message $GREEN "✔ $msg"
-done
-for msg in "${ERRORS[@]}"; do
-    print_message $RED "✖ $msg"
-done
+source ${MAIN_DIR}/venv2/bin/activate
+cd ${MAIN_DIR}/modules/sweyntooth
+pip install -r requirements.txt
+cd libs/smp_server/
+make all && make install
+cd ../../
+deactivate
+
+source ${MAIN_DIR}/venv3/bin/activate
+sudo rm -rf /var/lib/apt/lists/*
+cd ${MAIN_DIR}/helpers/pybluez
+pip install .
+cd ../../helpers/bluing
+env CFLAGS="-lm" LDFLAGS="-ldl -pthread -lutil" pip install .
+cd ../../
+pip install .
+deactivate
+
+cd $MAIN_DIR
+rm -rf $TMP_DIR
+
+echo "Setup complete. You can now use the environments as needed."
